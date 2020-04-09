8185c29d76d40f743737f35d010990409ea09811
burhanuddin78 <burhanuddin5023@gmail.com>
2020-04-10 00:13:14 +0500

    Updates

---
+++ precache-manifest.1cb0d42cf6d0c1cd4e7f941389a01794.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "053dc86b3b75336588e42232adc55e82",
+    "url": "/my-first-react-app/index.html"
+  },
+  {
+    "revision": "fba528dfe72b34060931",
+    "url": "/my-first-react-app/static/css/main.4ea1dff1.chunk.css"
+  },
+  {
+    "revision": "5b7a16d80aacfeed1077",
+    "url": "/my-first-react-app/static/js/2.4c831a2a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/my-first-react-app/static/js/2.4c831a2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fba528dfe72b34060931",
+    "url": "/my-first-react-app/static/js/main.f34c83a1.chunk.js"
+  },
+  {
+    "revision": "e6b4829c11c391ada93f",
+    "url": "/my-first-react-app/static/js/runtime-main.6f4aecde.js"
+  }
+]);
